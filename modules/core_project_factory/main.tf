900588feb7f07051432ddda1e09b73607d7e75b8
Dustin Johnson <dustin@i-johnson.com>
2025-10-18 21:32:44 -0700

    Fixing org_id bug in main project

--- modules/core_project_factory/main.tf
+++ modules/core_project_factory/main.tf
@@ -73,7 +73,6 @@ locals {
 resource "google_project" "main" {
   name                = var.name
   project_id          = local.temp_project_id
-  org_id              = ""
   folder_id           = local.project_folder_id
   billing_account     = var.billing_account
   auto_create_network = var.auto_create_network
